0de0930f1737f9d919baa73d5270f4bbe53a6a5b
Harel Grecht <Harel.grecht@gmail.com>
2026-02-05 19:26:59 +0200

    [ADD] Add gpio pin and serial prints to enable sensor state reading

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,24 @@
 #define LOW_GAS HIGH
 #define HIGH_GAS LOW
 #define statusLed 8
-#define sensorIn 10
+#define sensorPin 10
 
 
 void setup() {
-    pinMode(sensorIn, INPUT);
+    pinMode(sensorPin, INPUT);
+    pinMode(sensorPin, INPUT_PULLUP);
     pinMode(statusLed, OUTPUT);
+    Serial.begin(115200);
 }
 
 void loop() {
-    if(digitalRead(statusLed) == HIGH_GAS) {
-        digitalWrite(statusLed, LOW);  
+    if(digitalRead(sensorPin) == LOW) {
+        digitalWrite(statusLed, LOW);
+        Serial.println("No Gas (Closed) - LED ON");
     } else {
-        digitalWrite(statusLed, HIGH); 
+        digitalWrite(statusLed, HIGH);
+        Serial.println("Gas Detected (Open) - LED OFF");
     }
+    delay(100);
 }
 
